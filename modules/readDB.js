21c7daa549d97c27aaed238ddea59be7bc1e6ce3
Agil Mughni <agilmughni1@gmail.com>
2025-03-02 22:36:43 +0700

    fix: db path

--- modules/readDB.js
+++ modules/readDB.js
@@ -1,6 +1,7 @@
+const path = require("path");
 const fs = require("fs").promises;
 
-const FILE_PATH = "../databases/db_arsip_buku.json";
+const FILE_PATH = path.join(__dirname, "../databases/db_arsip_buku.json");
 
 const readDatabase = async () => {
     try {
